f898168492574478548c68b98ac1c49062be9e98
harshit <onxy.harsh123@gmail.com>
2026-06-19 22:49:55 +0530

    the terraform files are updated and bugs are fixed

--- backend/scripts/terraform/aws/resources/server/security_group.tf
+++ backend/scripts/terraform/aws/resources/server/security_group.tf
@@ -14,11 +14,11 @@ resource "aws_security_group" "security-grp1" {
     }
   }
 
-  dynamic "egress" {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    
+
   }
 }
