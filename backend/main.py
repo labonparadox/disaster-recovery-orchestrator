51a463dd97dd5f00d914e5c2564fb16d1840fecc
shrutii3020 <shrutisingh1379@gmail.com>
2026-06-20 07:34:46 +0530

    Fix main.py imports and FastAPI app setup

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
+
 from backend.services.login import router as login
 from backend.services.signup import router as signup
 from backend.health.health import router as health
-app = FastAPI()
 
+app = FastAPI()
 
 app.include_router(login)
 app.include_router(signup)
 app.include_router(health)
+
 
 @app.get("/")
 def home():
